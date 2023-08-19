2253288dd40d9ad0c75a93e150d85d3e29d40ef9
NourNabil2 <nour60g@gmail.com>
2023-08-19 20:12:34 +0300

    first_push

---
+++ lib/Trick_1/button_UI.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Button extends StatefulWidget {
+  const Button({super.key});
+
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+
+        ),
+      ),
+    );
+  }
+}
